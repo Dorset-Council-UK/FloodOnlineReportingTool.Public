2e7fad5c78d7be0e4b2dc623c32a4d468aa35b56
weolo <matt.mailuk@gmail.com>
2025-07-15 12:25:49 +0100

    Using the GdsInputNumber component for the flood duration page

--- FloodOnlineReportingTool.Public/Models/FloodReport/Create/FloodDuration.cs
+++ FloodOnlineReportingTool.Public/Models/FloodReport/Create/FloodDuration.cs
@@ -7,9 +7,11 @@ public class FloodDuration
     [GdsFieldErrorClass(GdsFieldTypes.Radio)]
     public Guid? DurationKnownId { get; set; }
 
+    [GdsFieldErrorClass(GdsFieldTypes.Input)]
     public string? DurationDaysText { get; set; }
     public int? DurationDaysNumber { get; set; }
 
+    [GdsFieldErrorClass(GdsFieldTypes.Input)]
     public string? DurationHoursText { get; set; }
     public int? DurationHoursNumber { get; set; }
 }
