22de5068bb4e025ffb0689bdfce48cb1fb75977a
weolo <matt.mailuk@gmail.com>
2025-08-04 15:42:48 +0100

    Configured Azure service bus connection and health checks

--- FloodOnlineReportingTool.Public/Extensions/HealthCheckExtensions.cs
+++ FloodOnlineReportingTool.Public/Extensions/HealthCheckExtensions.cs
@@ -4,52 +4,40 @@ using FloodOnlineReportingTool.Public.Settings;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using RabbitMQ.Client;
 
 namespace Microsoft.AspNetCore.Builder;
 
-internal static class FloodReportingHealthExtensions
+internal static class HealthCheckExtensions
 {
     private static readonly IEnumerable<string> tagsDatabase = ["database"];
-    private static readonly IEnumerable<string> tagsRabbitMQ = ["rabbitmq"];
+    private static readonly IEnumerable<string> tagsServiceBusQueue = ["messaging", "azure", "service bus", "queue"];
+    private static readonly IEnumerable<string> tagsServiceBusThreshold = ["messaging", "azure", "service bus", "threshold"];
+    private static readonly IEnumerable<string> tagsServiceBusSubscription = ["messaging", "azure", "service bus", "subscription"];
+    private static readonly IEnumerable<string> tagsServiceBusTopic = ["messaging", "azure", "service bus", "topic"];
     private static readonly IEnumerable<string> tagsAddressSearchAPI = ["api", "address", "search"];
     private static readonly IEnumerable<string> tagsAddressNearestAPI = ["api", "address", "nearest"];
     private static readonly IEnumerable<string> tagsLive = ["live"];
 
-    public static IServiceCollection AddFloodReportingHealthChecks(this IServiceCollection services, IConfigurationSection? rabbitMqSection)
+    public static IServiceCollection AddFloodReportingHealthChecks(this IServiceCollection services, MessagingSettings messagingSettings)
     {
-        services
+        var builder = services
             .AddHealthChecks()
             .AddDbContextCheck<FORTDbContext>(tags: tagsDatabase)
+            .AddDbContextCheck<UserDbContext>(tags: tagsDatabase)
             .AddDbContextCheck<BoundariesDbContext>(tags: tagsDatabase)
             .AddCheck<ApiAdvancedSearchHealthCheck>("ApiAdvancedSearch", tags: tagsAddressSearchAPI)
             .AddCheck<ApiNearestAddressesHealthCheck>("ApiNearestAddresses", tags: tagsAddressNearestAPI)
             // Add a default liveness check to ensure app is responsive
             .AddCheck("self", () => HealthCheckResult.Healthy(), tagsLive);
 
-        // Messaging options and health checks
-        if (rabbitMqSection is not null)
+        if (messagingSettings.Enabled)
         {
-            var rabbitMqSettings = rabbitMqSection.Get<RabbitMqSettings>();
-            if (rabbitMqSettings is not null && rabbitMqSettings.Enabled)
-            {
-                // Add a long lived RabbitMQ connection
-                services.AddSingleton<IConnection>(sp =>
-                {
-                    var factory = new ConnectionFactory
-                    {
-                        Uri = rabbitMqSettings.Host,
-                        UserName = rabbitMqSettings.Username,
-                        Password = rabbitMqSettings.Password,
-                    };
-                    return factory.CreateConnectionAsync().GetAwaiter().GetResult();
-                });
-
-                // Add RabbitMQ health checks
-                services
-                    .AddHealthChecks()
-                    .AddRabbitMQ(tags: tagsRabbitMQ);
-            }
+            // Add Azure Service Bus checks
+            builder
+                .AddAzureServiceBusQueue(messagingSettings.ConnectionString, messagingSettings.QueueName, tags: tagsServiceBusQueue)
+                .AddAzureServiceBusQueueMessageCountThreshold(messagingSettings.ConnectionString, messagingSettings.QueueName, tags: tagsServiceBusThreshold)
+                .AddAzureServiceBusSubscription(messagingSettings.ConnectionString, messagingSettings.TopicName, messagingSettings.SubscriptionName, tags: tagsServiceBusSubscription)
+                .AddAzureServiceBusTopic(messagingSettings.ConnectionString, messagingSettings.TopicName, tags: tagsServiceBusTopic);
         }
 
         return services;
---
+++ FloodOnlineReportingTool.Public/Settings/MessagingSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace FloodOnlineReportingTool.Public.Settings;
+
+public class MessagingSettings
+{
+    public const string SectionName = "Messaging";
+
+    public required bool Enabled { get; init; } = false;
+    public required string ConnectionString { get; init; }
+    public required string QueueName { get; init; }
+    public required string TopicName { get; init; }
+    public required string SubscriptionName { get; init; }
+}
