86eb17aab902a7fc6d38e326f970c0bf8f9a174d
weolo <matt.mailuk@gmail.com>
2025-08-15 17:00:15 +0100

    Improved KeyVault setup, calling it early with self contained logic.

--- FloodOnlineReportingTool.Public/Extensions/SettingsExtensions.cs
+++ FloodOnlineReportingTool.Public/Extensions/SettingsExtensions.cs
@@ -7,12 +7,11 @@ namespace Microsoft.AspNetCore.Builder;
 
 internal static class SettingsExtensions
 {
-    internal static (KeyVaultSettings?, MessagingSettings, GISSettings, MicrosoftIdentityOptions?) AddFloodReportingSettings(this IServiceCollection services, IConfigurationManager configuration)
+    internal static (MessagingSettings, GISSettings, MicrosoftIdentityOptions?) AddFloodReportingSettings(this IServiceCollection services, IConfigurationManager configuration)
     {
         // Sections
         var gisSection = configuration.GetSection(GISSettings.SectionName)
             ?? throw new ConfigurationMissingException($"Configuration section '{GISSettings.SectionName}' is missing.");
-        var keyVaultSection = gisSection.GetSection(KeyVaultSettings.SectionName);
         var messagingSection = configuration.GetRequiredSection(MessagingSettings.SectionName)
             ?? throw new ConfigurationMissingException($"Configuration section '{MessagingSettings.SectionName}' is missing.");
         var identitySection = configuration.GetSection(Constants.AzureAd);
@@ -20,20 +19,14 @@ internal static class SettingsExtensions
         // Configure settings
         services.Configure<GISSettings>(gisSection);
         services.Configure<MessagingSettings>(messagingSection);
-        var hasKeyVaultSettings = keyVaultSection.Value != null;
-        if (hasKeyVaultSettings)
-        {
-            services.Configure<KeyVaultSettings>(keyVaultSection);
-        }
 
         // Settings
-        var keyVaultSettings = hasKeyVaultSettings ? keyVaultSection.Get<KeyVaultSettings>() : null;
         var messagingSettings = messagingSection.Get<MessagingSettings>()
             ?? throw new ConfigurationMissingException($"Configuration section '{MessagingSettings.SectionName}' is not properly defined.");
         var gisSettings = gisSection.Get<GISSettings>()
             ?? throw new ConfigurationMissingException($"Configuration section '{GISSettings.SectionName}' is not properly defined.");
         MicrosoftIdentityOptions? identityOptions = identitySection?.Get<MicrosoftIdentityOptions>();
 
-        return (keyVaultSettings, messagingSettings, gisSettings, identityOptions);
+        return (messagingSettings, gisSettings, identityOptions);
     }
 }
