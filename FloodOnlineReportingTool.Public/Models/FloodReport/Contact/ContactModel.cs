6409b6feacebd6ee418eccb3e53e68eb6b2a8241
Paul Wittle <paul.wittle@dorsetcouncil.gov.uk>
2025-12-16 16:31:17 +0000

    Add contact form validation

--- FloodOnlineReportingTool.Public/Models/FloodReport/Contact/ContactModel.cs
+++ FloodOnlineReportingTool.Public/Models/FloodReport/Contact/ContactModel.cs
@@ -1,26 +1,22 @@
 ﻿using FloodOnlineReportingTool.Contracts.Shared;
-using GdsBlazorComponents;
 
 namespace FloodOnlineReportingTool.Public.Models.FloodReport.Contact;
 
 public class ContactModel
 {
-    [GdsFieldErrorClass(GdsFieldTypes.Radio)]
-    public ContactRecordType? ContactType { get; set; }
+    
+    public ContactRecordType? ContactType { get; set; } 
 
-    [GdsFieldErrorClass(GdsFieldTypes.Input)]
-    public string? ContactName { get; set; }
+    public string? ContactName { get; set; } 
 
-    [GdsFieldErrorClass(GdsFieldTypes.Input)]
-    public string? EmailAddress { get; set; }
+    public string? EmailAddress { get; set; } 
+    public bool IsRecordOwner { get; set; } = false;
 
-    public bool IsEmailVerified { get; init; } // Read-only in this view model
+    public bool IsEmailVerified { get; set; } = false;
 
-    [GdsFieldErrorClass(GdsFieldTypes.Input)]
-    public string? PhoneNumber { get; set; }
+    public bool IsSubscribed { get; init; } // Read-only in this view model
 
-    [GdsFieldErrorClass(GdsFieldTypes.Radio)]
-    public bool PrimaryContactRecord { get; set; }
+    public string? PhoneNumber { get; set; } 
 
     public Guid? Id { get; set; }
 
