eec5bfc0fa56a25f536e84b9fcf5fd773463093c
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-03-24 17:06:22 +0000

    New eligibility check component to work with Entity and Dto (work in progress)

--- FloodOnlineReportingTool.Public/Components/EligibilityCheckSummary.razor.cs
+++ FloodOnlineReportingTool.Public/Components/EligibilityCheckSummary.razor.cs
@@ -4,18 +4,25 @@ using FloodOnlineReportingTool.Database.Repositories;
 using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace FloodOnlineReportingTool.Public.Components;
 
-public partial class EligibilityCheckDtoSummary(
-    ILogger<EligibilityCheckDtoSummary> logger,
+public partial class EligibilityCheckSummary(
+    ILogger<EligibilityCheckSummary> logger,
     ICommonRepository commonRepository,
-    IValidator<EligibilityCheckDto> validator
+    IValidator<EligibilityCheck> validatorEntity,
+    IValidator<EligibilityCheckDto> validatorDto
 ) : IAsyncDisposable
 {
-    [Parameter, EditorRequired]
-    public EligibilityCheckDto EligibilityCheckDto { get; set; }
-    private EligibilityCheckDto? _previousEligibilityCheckDto;
+    [Parameter]
+    public EligibilityCheckDto? Dto { get; set; }
+    //private bool HasDto => Dto is not null;
+
+    [Parameter]
+    public EligibilityCheck? Entity { get; set; }
+    //private bool HasEntity => Entity is not null;
+    //private EligibilityCheck? _eligibilityCheck;
 
     [Parameter]
     public EventCallback<bool> ValidationStatusChanged { get; set; }
@@ -26,6 +33,26 @@ public partial class EligibilityCheckDtoSummary(
 
     [PersistentState(AllowUpdates = true)]
     public IReadOnlyCollection<FloodImpact>? EligibilityCheckFloodImpacts { get; set; }
+
+    // Id
+    [Parameter]
+    public bool ShowId { get; set; } = false;
+    private string? _idLabel;
+
+    // Created date
+    [Parameter]
+    public bool ShowCreatedDate { get; set; } = true;
+    private string? _createdDateLabel;
+
+    // Updated date
+    [Parameter]
+    public bool ShowUpdatedDate { get; set; } = true;
+    private string? _updatedDateLabel;
+
+    // Terms agreed
+    [Parameter]
+    public bool ShowTermsAgreed { get; set; } = true;
+    private string? _termsAgreedLabel;
 
     // Is address / Is postal address
     [Parameter]
@@ -50,7 +77,7 @@ public partial class EligibilityCheckDtoSummary(
     // Is uninhabitable
     [Parameter]
     public bool ShowIsUninhabitable { get; set; } = true;
-    private bool? _isUninhabitable;
+    private string? _isUninhabitableLabel;
 
     // Temporary location description
     [Parameter]
@@ -98,12 +125,36 @@ public partial class EligibilityCheckDtoSummary(
 
     protected override async Task OnParametersSetAsync()
     {
-        var parametersUnchanged = ReferenceEquals(_previousEligibilityCheckDto, EligibilityCheckDto);
-        if (parametersUnchanged)
+        if (Dto is null && Entity is null)
         {
-            return;
+            throw new InvalidOperationException("EligibilityCheckSummary component requires either a Dto or an Entity parameter.");
         }
-        _previousEligibilityCheckDto = EligibilityCheckDto;
+
+        if (Dto is not null && Entity is not null)
+        {
+            throw new InvalidOperationException("EligibilityCheckSummary component shouldn't have both Dto and Entity parameters set.");
+        }
+
+        //if (HasEntity)
+        //{
+        //    // use the entity if we have it
+        //    _eligibilityCheck = Entity;
+        //}
+        //else if (HasDto)
+        //{
+        //    // otherwise create a new eligibility check from the dto.
+        //    // ignoring the id, created date, updated date, and terms agreed data, as they are not relevant for a DTO summary
+        //    var anyGuid = Guid.Empty;
+        //    var anyDate = DateTimeOffset.MinValue;
+        //    var impactDuration = await Dto.CalculateImpactDurationHours(contextFromFactory??, _cts.Token);
+        //    _eligibilityCheck = Dto.ToCreatedEntity(anyGuid, anyDate, anyDate, impactDuration);
+        //}
+
+        // Eligibility check details
+        GetId();
+        GetCreatedDate();
+        GetUpdatedDate();
+        GetTermsAgreed();
 
         // Property details
         GetIsAddress();
@@ -176,7 +227,20 @@ public partial class EligibilityCheckDtoSummary(
 
     private async Task ValidateAsync()
     {
-        var validationResult = await validator.ValidateAsync(EligibilityCheckDto, _cts.Token);
+        ValidationResult? validationResult;
+        if (Entity is not null)
+        {
+            validationResult = await validatorEntity.ValidateAsync(Entity, _cts.Token);
+        }
+        else if (Dto is not null)
+        {
+            validationResult = await validatorDto.ValidateAsync(Dto, _cts.Token);
+        }
+        else
+        {
+            return;
+        }
+
         _validationFailures = validationResult.Errors;
 
         await ValidationStatusChanged.InvokeAsync(validationResult.IsValid);
@@ -230,6 +294,144 @@ public partial class EligibilityCheckDtoSummary(
 
         return [.. EligibilityCheckFloodImpacts
             .Where(o => ids.Contains(o.Id))
+            .Select(o => o.TypeName ?? Unknown),
+        ];
+    }
+
+    private void GetId()
+    {
+        if (!ShowId || Entity is null)
+        {
+            _idLabel = null;
+            return;
+        }
+        
+        _idLabel = Entity.Id.ToString();
+    }
+
+    private void GetCreatedDate()
+    {
+        if (!ShowCreatedDate || Entity is null)
+        {
+            _createdDateLabel = null;
+            return;
+        }
+        
+        _createdDateLabel = Entity.CreatedUtc.GdsReadable();
+    }
+
+    private void GetUpdatedDate()
+    {
+        if (!ShowUpdatedDate || Entity is null)
+        {
+            _updatedDateLabel = null;
+            return;
+        }
+
+        _updatedDateLabel = Entity.UpdatedUtc.GdsReadable();
+    }
+
+    private void GetTermsAgreed()
+    {
+        if (!ShowTermsAgreed || Entity is null)
+        {
+            _termsAgreedLabel = null;
+            return;
+        }
+
+        _termsAgreedLabel = Entity.TermsAgreed.GdsReadable();
+    }
+
+    private void GetIsAddress()
+    {
+        if (!ShowIsAddress)
+        {
+            _isAddressLabel = null;
+            return;
+        }
+
+        _isAddressLabel = (Entity?.IsAddress ?? Dto?.IsAddress) switch
+        {
+            true => "Yes",
+            false => "No",
+            null => Unknown,
+        };
+    }
+
+    private void GetLocationDescription()
+    {
+        if (!ShowLocationDescription)
+        {
+            _locationDescriptionLabel = null;
+            return;
+        }
+
+        _locationDescriptionLabel = Entity?.LocationDesc ?? Dto?.LocationDesc ?? Unknown;
+    }
+
+    private void GetPropertyType()
+    {
+        if (!ShowPropertyType)
+        {
+            _propertyTypeLabel = null;
+            return;
+        }
+
+        _propertyTypeLabel = Unknown;
+    }
+
+    private void GetFloodedAreas()
+    {
+        if (!ShowFloodedAreas)
+        {
+            _floodedAreaLabels = [];
+            return;
+        }
+
+        // get the flood impact ids from the entity or dto
+        HashSet<Guid> floodImpactIds = [];
+        if (Entity is not null)
+        {
+            foreach (var id in Entity.Residentials.Select(o => o.FloodImpactId))
+            {
+                floodImpactIds.Add(id);
+            }
+
+            foreach (var id in Entity.Commercials.Select(o => o.FloodImpactId))
+            {
+                floodImpactIds.Add(id);
+            }
+        }
+        else if (Dto is not null)
+        {
+            foreach (var id in Dto.Residentials)
+            {
+                floodImpactIds.Add(id);
+            }
+
+            foreach (var id in Dto.Commercials)
+            {
+                floodImpactIds.Add(id);
+            }
+        }
+
+        if (floodImpactIds.Count == 0)
+        {
+            _floodedAreaLabels = [];
+            return;
+        }
+
+        if (EligibilityCheckFloodImpacts is null || EligibilityCheckFloodImpacts.Count == 0)
+        {
+            _floodedAreaLabels = [Unknown];
+            return;
+        }
+
+        // get the unique, sorted, flood impact labels
+        _floodedAreaLabels = [.. EligibilityCheckFloodImpacts
+            .Where(o => floodImpactIds.Contains(o.Id))
+            .DistinctBy(o => o.Id)
+            .OrderBy(o => o.OptionOrder)
             .Select(o => o.TypeName ?? Unknown),
         ];
     }
