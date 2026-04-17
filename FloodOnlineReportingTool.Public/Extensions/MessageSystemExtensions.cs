00789febd55f2ec352678147f51db2e94c8690a4
Paul Wittle <paul.wittle@dorsetcouncil.gov.uk>
2026-04-17 08:41:38 +0100

    Remove temporary extra debugging.

--- FloodOnlineReportingTool.Public/Extensions/MessageSystemExtensions.cs
+++ FloodOnlineReportingTool.Public/Extensions/MessageSystemExtensions.cs
@@ -57,36 +57,10 @@ internal static class MessageSystemExtensions
                     {
                         m.SetEntityName(TopicNames.FloodSourceCreated);  // Use the exact topic name from Azure
                     });
-
-                    // Add diagnostics
-                    config.UseSendFilter(typeof(LoggingSendFilter<>), context);
                 });
             });
 
             return builder;
         }
     }
-}
-
-internal class LoggingSendFilter<T> : IFilter<SendContext<T>> where T : class
-{
-    private readonly ILogger<LoggingSendFilter<T>> _logger;
-
-    public LoggingSendFilter(ILogger<LoggingSendFilter<T>> logger)
-    {
-        _logger = logger;
-    }
-
-    public async Task Send(SendContext<T> context, IPipe<SendContext<T>> next)
-    {
-        _logger.LogInformation(
-            "Sending message: {MessageType}, Destination: {Destination}, MessageId: {MessageId}",
-            typeof(T).Name,
-            context.DestinationAddress,
-            context.MessageId);
-
-        await next.Send(context);
-    }
-
-    public void Probe(ProbeContext context) { }
 }
