145ddc829eeece8f24eb3ba1d4a21276f648a6b9
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-04-24 14:13:05 +0100

    Updated overview page

--- FloodOnlineReportingTool.Public/Components/Pages/FloodReport/Overview/Index.razor.cs
+++ FloodOnlineReportingTool.Public/Components/Pages/FloodReport/Overview/Index.razor.cs
@@ -1,8 +1,8 @@
-﻿using FloodOnlineReportingTool.Database.Models.Eligibility;
-using FloodOnlineReportingTool.Database.Repositories;
+﻿using FloodOnlineReportingTool.Database.Repositories;
+using FloodOnlineReportingTool.Public.Authentication;
 using FloodOnlineReportingTool.Public.Models.Order;
 using FloodOnlineReportingTool.Public.Services;
-using GdsBlazorComponents;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
@@ -11,38 +11,23 @@ namespace FloodOnlineReportingTool.Public.Components.Pages.FloodReport.Overview;
 
 public partial class Index(
     IFloodReportRepository floodReportRepository,
+    IAuthorizationService authorizationService,
     NavigationManager navigationManager,
     SessionStateService scopedSessionStorage
-) : IPageOrder, IAsyncDisposable
+) : IAsyncDisposable
 {
-    // Page order properties
-    public string Title { get; set; } = FloodReportPages.Overview.Title;
-    public IReadOnlyCollection<GdsBreadcrumb> Breadcrumbs { get; set; } = [GeneralPages.Home.ToGdsBreadcrumb()];
-
     [CascadingParameter]
     public Task<AuthenticationState>? AuthenticationState { get; set; }
 
-    [Parameter]
-    public Guid? FloodReportId { get; set; }
-    private Guid _floodReportId = Guid.Empty;
+    // Text which changes for users and admins
+    private string _h1TitleText = FloodReportPages.Overview.Title;
+    private string _manageText = "Manage your flood reports";
+    private string _noneFoundText = "We cannot find your flood reports.";
 
     private readonly CancellationTokenSource _cts = new();
     private bool _isLoading = true;
-    private IList<Database.Models.Flood.FloodReport> _floodReports = [];
-    private bool _accessHasExpired = true;
-    private TimeSpan _accessTimeLeft;
-
-    // These are returning information about the current record
-    private EligibilityOptions _floodInvestigation;
-    //private IList<Organisation> _leadLocalFloodAuthorities = [];
-    //private IList<Organisation> _otherFloodAuthorities = [];
-
-    // These don't have any logic yet in the repository
-    private bool _isEmergencyResponse;
-    private string? _section19Url;
-    private EligibilityOptions _grantApplication;
-    private EligibilityOptions _propertyProtection;
-    private EligibilityOptions _section19;
+    private IReadOnlyCollection<Database.Models.Flood.FloodReport> _floodReports = [];
+    private readonly string _signInUrl = $"{AccountPages.SignIn.Url}?redirectUri={navigationManager.SignInRedirectUri}";
 
     public async ValueTask DisposeAsync()
     {
@@ -58,78 +43,124 @@ public partial class Index(
         GC.SuppressFinalize(this);
     }
 
-    protected override async Task OnParametersSetAsync()
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        string? userId = null;
-        if (AuthenticationState is not null)
+        if (firstRender)
         {
-            var authState = await AuthenticationState;
-            userId = authState.User.Oid;
-        }
-        if (userId is null)
-        {
-            _floodReportId = FloodReportId ?? await scopedSessionStorage.GetFloodReportId();
-            var localReport = await floodReportRepository.GetById(_floodReportId, _cts.Token);
-            if (localReport is null)
+            /*
+             * scenarios to handle:
+             * Not authenicated
+             * Not authenticated with flood report ID in protected session storage
+             * Authenticated as an admin
+             * Authenticated as a user
+             */
+
+            if (AuthenticationState is not null)
             {
-                _isLoading = false;
-                return;
+                var authState = await AuthenticationState;
+                if (authState.User.IsAuthenticated)
+                {
+                    var adminPolicyCheck = await authorizationService.AuthorizeAsync(authState.User, PolicyNames.Admin);
+                    var hasAdminPolicy = adminPolicyCheck.Succeeded;
+
+                    _floodReports = hasAdminPolicy
+                        ? await GetAdminsFloodReports(authState, hasAdminPolicy)
+                        : await GetCurrentUsersFloodReports(authState, hasAdminPolicy);
+                }
+                else
+                {
+                    _floodReports = await GetStoredFloodReports(authState);
+                }
             }
-            _floodReports.Add((Database.Models.Flood.FloodReport)localReport);
-        }
-        else
-        {
-            _floodReports = [.. await floodReportRepository.AllReportedByContact(userId, _cts.Token)];
-        }
 
-        if (_floodReports.Count > 0)
-        {
-            //var result = await floodReportRepository.CalculateEligibilityWithReference(_floodReports.Reference, _cts.Token);
-
-            ////_leadLocalFloodAuthorities = [.. result.ResponsibleOrganisations.Where(o => o.FloodAuthorityId == FloodAuthorityIds.LeadLocalFloodAuthority)];
-            ////_otherFloodAuthorities = [.. result.ResponsibleOrganisations.Where(o => o.FloodAuthorityId != FloodAuthorityIds.LeadLocalFloodAuthority)];
-
-            //// These don't have any logic yet in the repository
-            //_floodInvestigation = result.FloodInvestigation;
-            //_isEmergencyResponse = result.IsEmergencyResponse;
-            //_section19Url = result.Section19Url;
-            //_section19 = result.Section19;
-            //_propertyProtection = result.PropertyProtection;
-            //_grantApplication = result.GrantApplication;
-
-            //// Check if the users access has expired
-            //if (_floodReport.ReportOwnerAccessUntil != null)
-            //{
-            //    _accessTimeLeft = _floodReport.ReportOwnerAccessUntil.Value - DateTimeOffset.UtcNow;
-            //    _accessHasExpired = _accessTimeLeft <= TimeSpan.Zero;
-            //}
-        }
-
-        _isLoading = false;
-        StateHasChanged();
-    }
-
-    private void PerformAction(Guid FloodReportId)
-    {
-        navigationManager.NavigateTo($"{InvestigationPages.Home.Url}/{FloodReportId}");
-        StateHasChanged();
-        return;
-    }
-
-    private void ViewReport(Guid FloodReportId)
-    {
-        navigationManager.NavigateTo($"{FloodReportPages.Details.Url}/{FloodReportId}");
-        StateHasChanged();
-        return;
-    }
-
-    private void UpdateReport(Guid? EligibilityCheckId)
-    {
-        if (EligibilityCheckId.HasValue)
-        {
-            navigationManager.NavigateTo($"{FloodReportPages.Update.Url}/{EligibilityCheckId.Value}");
+            _isLoading = false;
             StateHasChanged();
         }
-        return;
+    }
+
+    private void PerformAction(Guid floodReportId)
+    {
+        navigationManager.NavigateTo($"{InvestigationPages.Home.Url}/{floodReportId}");
+    }
+
+    private void ViewReport(Guid floodReportId)
+    {
+        navigationManager.NavigateTo($"{FloodReportPages.Details.Url}/{floodReportId}");
+    }
+
+    private void UpdateReport(Guid? eligibilityCheckId)
+    {
+        if (eligibilityCheckId.HasValue)
+        {
+            navigationManager.NavigateTo($"{FloodReportPages.Update.Url}/{eligibilityCheckId.Value}");
+        }
+    }
+
+    /// <summary>
+    /// Gets all flood reports
+    /// </summary>
+    /// <remarks>The user has to be authenticated, and an admin.</remarks>
+    private async Task<IReadOnlyCollection<Database.Models.Flood.FloodReport>> GetAdminsFloodReports(AuthenticationState authState, bool hasAdminPolicy)
+    {
+        if (!authState.User.IsAuthenticated || !hasAdminPolicy)
+        {
+            return [];
+        }
+
+        // Update the title, manage text, and none found text for admins
+        _h1TitleText = "View all flood reports";
+        _manageText = "Manage all flood reports";
+        _noneFoundText = "We cannot find any flood reports.";
+
+        return await floodReportRepository.GetAllOverview(_cts.Token);
+    }
+
+    /// <summary>
+    /// Get the current users floods reports
+    /// </summary>
+    /// <remarks>The user has to be authenticated, and NOT an admin.</remarks>
+    private async Task<IReadOnlyCollection<Database.Models.Flood.FloodReport>> GetCurrentUsersFloodReports(AuthenticationState authState, bool hasAdminPolicy)
+    {
+        if (!authState.User.IsAuthenticated || hasAdminPolicy)
+        {
+            return [];
+        }
+
+        string? userId = authState.User.Oid;
+        if (userId is null)
+        {
+            return [];
+        }
+
+        return await floodReportRepository.AllReportedByContact(userId, _cts.Token);
+    }
+
+    /// <summary>
+    /// Get the stored flood report
+    /// </summary>
+    /// <remarks>
+    ///     <para>The user has to not be authenticated. Flood report ID exists in protected storage.</para>
+    ///     <para>This can happen from the /floodreport/create/confirmation page</para>
+    /// </remarks>
+    private async Task<IReadOnlyCollection<Database.Models.Flood.FloodReport>> GetStoredFloodReports(AuthenticationState authState)
+    {
+        if (authState.User.IsAuthenticated)
+        {
+            return [];
+        }
+
+        var floodReportId = await scopedSessionStorage.GetFloodReportId();
+        if (floodReportId == Guid.Empty)
+        {
+            return [];
+        }
+
+        var floodReport = await floodReportRepository.GetById(floodReportId, _cts.Token);
+        if (floodReport is null)
+        {
+            return [];
+        }
+
+        return [floodReport];
     }
 }
