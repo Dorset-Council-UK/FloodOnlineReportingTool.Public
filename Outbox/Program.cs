607df6f7f76cea1f1c31299a7040fbfa55347ebe
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-05-14 16:14:59 +0100

    using Aspire libraries for postgres and service bus in outbox

--- Outbox/Program.cs
+++ Outbox/Program.cs
@@ -1,8 +1,25 @@
+using FloodOnlineReportingTool.Database.DbContexts;
+using Microsoft.EntityFrameworkCore;
 using Outbox;
+using ServiceDefaults;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
+
+//string? connectionString = builder.Configuration.GetConnectionString(ConnectionStringNames.Public);
+//if (string.IsNullOrWhiteSpace(connectionString))
+//{
+//    throw new Exception($"Missing configuration setting: The database connection string '{ConnectionStringNames.Public}' is missing.");
+//}
+builder.Services.AddDbContextPool<PublicDbContext>(options => options.UseNpgsql((string?)null));
+builder.EnrichNpgsqlDbContext<PublicDbContext>();
+
+//if (string.IsNullOrWhiteSpace(builder.Configuration.GetConnectionString(ConnectionStringNames.ServiceBus)))
+//{
+//    throw new Exception($"Missing configuration setting: The service bus connection string '{ConnectionStringNames.ServiceBus}' is missing.");
+//}
+builder.AddAzureServiceBusClient(ConnectionStringNames.ServiceBus);
 
 await builder.Build().RunAsync();
