5a4539260ee67efa6d0fe7a71fbc3062c7f14936
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-05-18 16:20:05 +0100

    more outbox message indexes

--- Database/Migrations/20260518151854_OutboxIndexes2.Designer.cs
+++ Database/Migrations/20260518151854_OutboxIndexes2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FloodOnlineReportingTool.Database.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FloodOnlineReportingTool.Database.Migrations
 {
     [DbContext(typeof(PublicDbContext))]
-    partial class PublicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260518151854_OutboxIndexes2")]
+    partial class OutboxIndexes2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Database/Migrations/20260518151854_OutboxIndexes2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FloodOnlineReportingTool.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutboxIndexes2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessages_Priority_Created",
+                schema: "fortpublic",
+                table: "OutboxMessages",
+                columns: new[] { "Priority", "Created" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboxMessages_Priority_Created",
+                schema: "fortpublic",
+                table: "OutboxMessages");
+        }
+    }
+}
