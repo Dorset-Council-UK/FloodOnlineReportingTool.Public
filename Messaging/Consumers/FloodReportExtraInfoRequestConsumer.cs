18aeda514df2cd5dbba7da6f1a25bf5b7f716a39
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-07-10 09:25:34 +0100

    Updated consumer

--- Messaging/Consumers/FloodReportExtraInfoRequestConsumer.cs
+++ Messaging/Consumers/FloodReportExtraInfoRequestConsumer.cs
@@ -7,7 +7,7 @@ using System.Text.Json;
 
 namespace Messaging.Consumers;
 
-internal sealed class FloodReportExtraInfoRequestConsumer(
+public sealed class FloodReportExtraInfoRequestConsumer(
     ILogger<FloodReportExtraInfoRequestConsumer> logger,
     IConfiguration configuration,
     IServiceScopeFactory scopeFactory
@@ -15,7 +15,7 @@ internal sealed class FloodReportExtraInfoRequestConsumer(
 {
     public string ContractName => nameof(FloodReportExtraInfoRequest);
 
-    public string TopicName => $"{TopicNames.FloodReportExtraInfoRequest}{configuration["Messaging:TopicSuffix"]}";
+    public string TopicName => $"{TopicNames.FloodReportExtraInfoRequest}{configuration["Messaging:TopicSuffix"]?.Trim()}";
 
     public ServiceBusProcessorOptions ProcessorOptions => new()
     {
@@ -39,24 +39,30 @@ internal sealed class FloodReportExtraInfoRequestConsumer(
                 return;
             }
 
-            //// InboxWorker and Consumers are singletons, so we have to create a scope to our service
-            //await using var scope = scopeFactory.CreateAsyncScope();
-            //IFloodReportSourceRepository floodReportSourceRepository = scope.ServiceProvider.GetRequiredService<IFloodReportSourceRepository>();
-            //var result = await floodReportSourceRepository.Update(message.Reference, RecordStatusIds.ActionNeeded, args.CancellationToken);
+            // InboxWorker and Consumers are singletons, so we have to create a scope to our service
+            await using var scope = scopeFactory.CreateAsyncScope();
+            IFloodReportSourceRepository floodReportSourceRepository = scope.ServiceProvider.GetRequiredService<IFloodReportSourceRepository>();
+            var result = await floodReportSourceRepository.Update(message.Reference, RecordStatusIds.ActionNeeded, args.CancellationToken);
 
-            //await args.CompleteMessageAsync(args.Message, args.CancellationToken);
+            if (!result.IsSuccess)
+            {
+                logger.LogError("Updating {Contract} {MessageId} failed: {Errors}", ContractName, args.Message.MessageId, string.Join(", ", result.Errors));
+                await args.DeadLetterMessageAsync(args.Message, "Update failed", string.Join(", ", result.Errors), args.CancellationToken);
+                return;
+            }
 
+            await args.CompleteMessageAsync(args.Message, args.CancellationToken);
             logger.LogInformation("Processed {Contract} {MessageId}", ContractName, args.Message.MessageId);
         }
         catch (JsonException ex)
         {
             logger.LogError(ex, "Message could not be deserialized for {Contract} {MessageId}", ContractName, args.Message.MessageId);
-            await args.DeadLetterMessageAsync(args.Message, ex.Message, ex.InnerException?.Message ?? "", cancellationToken: args.CancellationToken);
+            await args.DeadLetterMessageAsync(args.Message, "JSON exception", ex.Message, args.CancellationToken);
         }
         catch (Exception ex)
         {
             logger.LogError(ex, "Error processing {Contract} {MessageId}", ContractName, args.Message.MessageId);
-            await args.DeadLetterMessageAsync(args.Message, ex.Message, ex.InnerException?.Message ?? "", cancellationToken: args.CancellationToken);
+            await args.DeadLetterMessageAsync(args.Message, "Unexpected exception", ex.Message, args.CancellationToken);
         }
     }
 
