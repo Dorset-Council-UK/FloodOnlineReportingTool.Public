405986c5af1b4b3a3c31ef4686089ed781b3047a
Daniel Ambrose <daniel.ambrose@dorsetcouncil.gov.uk>
2026-08-07 16:35:05 +0100

    #211 Continued adding data for displaying on flood investigation summary tab component.

--- FloodOnlineReportingTool.Public/Components/InvestigationSummary.razor.cs
+++ FloodOnlineReportingTool.Public/Components/InvestigationSummary.razor.cs
@@ -1,16 +1,18 @@
-﻿using FloodOnlineReportingTool.Database.Models.Investigate;
+﻿using Azure.Identity;
+using FloodOnlineReportingTool.Database.Models.Flood;
+using FloodOnlineReportingTool.Database.Models.Investigate;
 using FloodOnlineReportingTool.Database.Repositories;
 using Microsoft.AspNetCore.Components;
 
 namespace FloodOnlineReportingTool.Public.Components;
 
-public partial class InvestigationSummary(
-    ILogger<EligibilityCheckSummary> logger,
-    ICommonRepository commonRepository
-) : IAsyncDisposable
+public partial class InvestigationSummary : IAsyncDisposable
 {
     [Parameter, EditorRequired]
     public Investigation Entity { get; set; }
+
+    [Parameter]
+    public bool UseMetric { get; set; } = true;
 
     [Parameter]
     public bool ShowWaterSpeed { get; set; } = true;
@@ -90,8 +92,192 @@ public partial class InvestigationSummary(
     private bool ShowWarnings()
         => ShowHelpReceivedWarnings || ShowBeforeFloodingWarnings || ShowWarningSources || (ShowFloodlineWarnings && _isFloodlineWarning);
 
-
     private readonly CancellationTokenSource _cts = new();
+    private const string Unknown = "Unknown";
+
+    protected override void OnParametersSet()
+    {
+        GetWaterSpeed();
+        GetWaterDestination();
+        GetDamagedVehicles();
+        GetInternalHow();
+        GetInternalWhen();
+        GetPeakDepth();
+        GetServiceImpact();
+        GetCommunityImpact();
+        GetBlockages();
+        GetActionsTaken();
+        GetHistory();
+        GetInsurance();
+
+        // warnings
+        GetHelpReceivedWarnings();
+        GetBeforeTheFloodingWarnings();
+        GetWarningSources();
+        GetFloodlineWarnings();
+    }
+
+
+    private void GetWaterSpeed()
+    {
+        PopulateLabel(ShowWaterSpeed, ref _beginLabel, e => e.Begin.TypeName);
+        PopulateLabel(ShowWaterSpeed, ref _waterSpeedLabel, e => e.WaterSpeed.TypeName);
+        PopulateLabel(ShowWaterSpeed, ref _appearanceLabel, e => e.Appearance.TypeName);
+    }
+
+    private void GetWaterDestination()
+    {
+        PopulateLabels(ShowWaterDestination, ref _destinationLabels, Entity.Destinations, d => d.FloodProblem.TypeName);
+    }
+    
+    private void GetDamagedVehicles()
+    {
+        PopulateLabel(ShowDamagedVehicles, ref _vehiclesDamagedMessage, e => e.NumberOfVehiclesDamaged switch
+            {
+                null or 0 => null,
+                1 => "1 vehicle was damaged",
+                _ => $"{Entity.NumberOfVehiclesDamaged} vehicles were damaged"
+            }
+        );
+    }
+
+    private void GetInternalHow()
+    {
+        PopulateLabels(IsInternal && ShowInternalHow, ref _entryLabels, Entity.Entries, e => e.FloodProblem.TypeName);
+    }
+
+    private void GetInternalWhen()
+    {
+        PopulateLabel(IsInternal && ShowInternalWhen, ref _internalWhen, e => e.WhenWaterEnteredKnown?.Text);
+    }
+
+    private void GetPeakDepth()
+    {
+        //TODO: Outside depth, due to needing a map to display. 
+
+        // Reset all peak depth fields
+        _isPeakDepthKnown = false;
+        _peakDepthNotKnownMessage = null;
+        _peakDepthInsideMessage = null;
+        _peakDepthOutsideMessage = null;
+
+        if (!ShowPeakDepth)
+        {
+            return;
+        }
+
+        if(Entity.IsPeakDepthKnownId == FloodOnlineReportingTool.Database.Models.Status.RecordStatusIds.Yes)
+        {
+            
+
+            //_isPeakDepthKnown = true;
+            //int? cm = Entity.PeakInsideCentimetres;
+            //_peakDepthInsideMessage = (UseMetric, cm) switch
+            //{
+            //    (_, null) => null,
+            //    (true, 1) => "1 centimetre",
+            //    (true, < 100) => $"{cm} centimetres",
+            //    (true, _)
+            //    (false, int insideCentimetres) => $"{insideCentimetres / 2.54m:F2} inch{(insideCentimetres == 2.54m ? "" : "es")}",
+            //    _ => null
+            //};
+        }
+        else if(Entity.IsPeakDepthKnownId == FloodOnlineReportingTool.Database.Models.Status.RecordStatusIds.No)
+        {
+            _peakDepthNotKnownMessage = "Not known";
+        }
+        else
+        {
+            _peakDepthNotKnownMessage = Unknown;
+        }
+    }
+
+    private void GetServiceImpact()
+    {
+        PopulateLabels(ShowServiceImpacts, ref _serviceImpactLabels, Entity.ServiceImpacts, s => s.FloodImpact.TypeName);
+    }
+
+    private void GetCommunityImpact()
+    {
+        PopulateLabels(ShowCommunityImpacts, ref _communityImpactLabels, Entity.CommunityImpacts, c => c.FloodImpact.TypeName);
+    }
+
+    private void GetBlockages()
+    {
+        PopulateLabel(ShowBlockages, ref _blockagesKnownProblemsLabel, b => b.KnownProblemDetails);
+    }
+
+    private void GetActionsTaken()
+    {
+        PopulateLabels(ShowActionsTaken, ref _actionsTakenLabels, Entity.ActionsTaken, a => a.FloodMitigation.TypeName);
+    }
+
+    private void GetHistory()
+    {
+        PopulateLabel(ShowHistory, ref _historyOfFloodingLabel, h => h.HistoryOfFlooding.Text);
+    }
+
+    private void GetInsurance()
+    {
+        PopulateLabel(ShowInsurance, ref _propertyInsuredLabel, p => p.PropertyInsured.Text);
+    }
+
+    private void GetHelpReceivedWarnings()
+    {
+        PopulateLabels(ShowHelpReceivedWarnings, ref _helpReceivedLabels, Entity.HelpReceived, h => h.FloodMitigation.TypeName);
+    }
+
+    private void GetBeforeTheFloodingWarnings()
+    {
+        PopulateLabel(ShowBeforeFloodingWarnings, ref _registeredWithFloodlineLabel, r => r.Floodline.Text);
+        PopulateLabel(ShowBeforeFloodingWarnings, ref _otherWarningReceivedLabel, o => o.WarningReceived.Text);
+    }
+
+    private void GetWarningSources()
+    {
+        PopulateLabels(ShowWarningSources, ref _warningSourcesLabels, Entity.WarningSources, w => w.FloodMitigation.TypeName);
+    }
+
+    private void GetFloodlineWarnings()
+    {
+        _isFloodlineWarning = ShowFloodlineWarnings
+            && Entity.FloodlineId == FloodOnlineReportingTool.Database.Models.Status.RecordStatusIds.Yes
+            && Entity.WarningSources.Select(m => m.FloodMitigation.Id).ToList().Contains(FloodMitigationIds.FloodlineWarning);
+        PopulateLabel(_isFloodlineWarning, ref _warningTimelyLabel, w => w.WarningTimely?.Text);
+        PopulateLabel(_isFloodlineWarning, ref _warningAppropriateLabel, w => w.WarningAppropriate?.Text);
+    }   
+
+
+    private void PopulateLabel(bool showLabel, ref string? labelToPoulate, Func<Investigation, string?> generateLabelFunc)
+    {
+        if (!showLabel)
+        {
+            labelToPoulate = null;
+            return;
+        }
+
+        labelToPoulate = generateLabelFunc(Entity);
+    }
+
+    private void PopulateLabels<T>(bool showLabel, ref string[] labelsToPoulate, IList<T> listFromWhichToPopulate, Func<T, string?> generateLabelsFunc)
+    {
+        if (!showLabel)
+        {
+            labelsToPoulate = [];
+            return;
+        }
+
+        if (listFromWhichToPopulate.Count == 0)
+        {
+            labelsToPoulate = [Unknown];
+            return;
+        }
+
+        labelsToPoulate = [.. listFromWhichToPopulate
+            .Select(generateLabelsFunc)
+            .Select(x => x ?? Unknown)];
+    }
+
 
     public async ValueTask DisposeAsync()
     {
