a2077c084cdef6f4547364833691f07c860968bb
Daniel Ambrose <daniel.ambrose@dorsetcouncil.gov.uk>
2026-08-10 11:27:11 +0100

    Created and used code to convert centimetres into a string with different measurement formats.

--- FloodOnlineReportingTool.Public/Extensions/StringExtensions.cs
+++ FloodOnlineReportingTool.Public/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace System;
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Tests")]
+
+namespace System;
 
 internal static class StringExtensions
 {
@@ -20,5 +23,76 @@ internal static class StringExtensions
             2 => $"{itemArray[0]}{finalSeparator}{itemArray[1]}",
             _ => $"{string.Join(separator, itemArray[..^1])}{finalSeparator}{itemArray[^1]}",
         };
+    }
+
+
+    public enum MeasurementDisplayType
+    {
+        Centimetres,
+        MetresAndCentimetres,
+        Inches,
+        FeetAndInches
+    }
+
+    /// <summary>
+    /// Converts a measurement in centimetres to a display string in the specified format.
+    /// </summary>
+    /// <param name="measurementInCentimetres">The measurement in centimetres to convert.</param>
+    /// <param name="displayType">The format in which to display the measurement.</param>
+    /// <returns>A string representing the measurement in the specified format.</returns>
+    /// <example>When using MeasurementDisplayType.FeetAndInches, a value of 63 centimetres would be returned as "2 feet 1 inch".</example>
+    internal static string ConvertMeasurementToDisplayString(this int? measurementInCentimetres, MeasurementDisplayType displayType)
+    {
+        if (measurementInCentimetres is null)
+        {
+            return string.Empty;
+        }
+        if (measurementInCentimetres < 0)
+        {
+            return "cannot convert negative number";
+        }
+
+        int measurementInInches = Convert.ToInt32(measurementInCentimetres.Value / 2.54);
+
+        return displayType switch
+        { 
+            MeasurementDisplayType.Centimetres => 
+                measurementInCentimetres switch
+                {
+                    1 => "1 centimetre",
+                    _ => $"{measurementInCentimetres} centimetres"
+                },
+            MeasurementDisplayType.MetresAndCentimetres =>
+                measurementInCentimetres switch
+                {
+                    1 => "1 centimetre",
+                    < 100 => $"{measurementInCentimetres} centimetres",
+                    100 => $"1 metre",
+                    101 => $"1 metre 1 centimetre",
+                    < 200 => $"1 metre {measurementInCentimetres - 100} centimetres",
+                    _ when measurementInCentimetres % 100 == 0 => $"{measurementInCentimetres / 100} metres",
+                    _ when measurementInCentimetres % 100 == 1 => $"{measurementInCentimetres / 100} metres 1 centimetre",
+                    _ => $"{measurementInCentimetres / 100} metres {measurementInCentimetres % 100} centimetres"
+                },
+            MeasurementDisplayType.Inches =>
+                measurementInInches switch
+                {
+                    1 => "1 inch",
+                    _ => $"{measurementInInches} inches"
+                },
+            MeasurementDisplayType.FeetAndInches => 
+                measurementInInches switch
+                {
+                    1 => "1 inch",
+                    < 12 => $"{measurementInInches} inches",
+                    12 => $"1 foot",
+                    13 => $"1 foot 1 inch",
+                    < 24 => $"1 foot {measurementInInches - 12} inches",
+                    _ when measurementInInches % 12 == 0 => $"{measurementInInches / 12} feet",
+                    _ when measurementInInches % 12 == 1 => $"{measurementInInches / 12} feet 1 inch",
+                    _ => $"{measurementInInches / 12} feet {measurementInInches % 12} inches"
+                },
+            _ => string.Empty
+        };   
     }
 }
---
+++ Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using FloodOnlineReportingTool.Contracts.Topics;
+using Messaging.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Extensions;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData(null, StringExtensions.MeasurementDisplayType.Centimetres, "")]
+    [InlineData(-1, StringExtensions.MeasurementDisplayType.Centimetres, "cannot convert negative number")]
+    [InlineData(0, StringExtensions.MeasurementDisplayType.Centimetres, "0 centimetres")]
+    [InlineData(1, StringExtensions.MeasurementDisplayType.Centimetres, "1 centimetre")]
+    [InlineData(2, StringExtensions.MeasurementDisplayType.Centimetres, "2 centimetres")]
+    [InlineData(999, StringExtensions.MeasurementDisplayType.Centimetres, "999 centimetres")]
+    [InlineData(null, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "")]
+    [InlineData(-1, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "cannot convert negative number")]
+    [InlineData(0, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "0 centimetres")]
+    [InlineData(1, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "1 centimetre")]
+    [InlineData(2, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "2 centimetres")]
+    [InlineData(99, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "99 centimetres")]
+    [InlineData(100, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "1 metre")]
+    [InlineData(101, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "1 metre 1 centimetre")]
+    [InlineData(199, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "1 metre 99 centimetres")]
+    [InlineData(200, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "2 metres")]
+    [InlineData(201, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "2 metres 1 centimetre")]
+    [InlineData(299, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "2 metres 99 centimetres")]
+    [InlineData(999, StringExtensions.MeasurementDisplayType.MetresAndCentimetres, "9 metres 99 centimetres")]
+    [InlineData(null, StringExtensions.MeasurementDisplayType.Inches, "")]
+    [InlineData(-1, StringExtensions.MeasurementDisplayType.Inches, "cannot convert negative number")]
+    [InlineData(0, StringExtensions.MeasurementDisplayType.Inches, "0 inches")]
+    [InlineData(1, StringExtensions.MeasurementDisplayType.Inches, "0 inches")]
+    [InlineData(2, StringExtensions.MeasurementDisplayType.Inches, "1 inch")]
+    [InlineData(3, StringExtensions.MeasurementDisplayType.Inches, "1 inch")]
+    [InlineData(5, StringExtensions.MeasurementDisplayType.Inches, "2 inches")]
+    [InlineData(999, StringExtensions.MeasurementDisplayType.Inches, "393 inches")]
+    [InlineData(null, StringExtensions.MeasurementDisplayType.FeetAndInches, "")]
+    [InlineData(-1, StringExtensions.MeasurementDisplayType.FeetAndInches, "cannot convert negative number")]
+    [InlineData(0, StringExtensions.MeasurementDisplayType.FeetAndInches, "0 inches")]
+    [InlineData(1, StringExtensions.MeasurementDisplayType.FeetAndInches, "0 inches")]
+    [InlineData(2, StringExtensions.MeasurementDisplayType.FeetAndInches, "1 inch")]
+    [InlineData(3, StringExtensions.MeasurementDisplayType.FeetAndInches, "1 inch")]
+    [InlineData(5, StringExtensions.MeasurementDisplayType.FeetAndInches, "2 inches")]
+    [InlineData(31, StringExtensions.MeasurementDisplayType.FeetAndInches, "1 foot")]
+    [InlineData(33, StringExtensions.MeasurementDisplayType.FeetAndInches, "1 foot 1 inch")]
+    [InlineData(58, StringExtensions.MeasurementDisplayType.FeetAndInches, "1 foot 11 inches")]
+    [InlineData(61, StringExtensions.MeasurementDisplayType.FeetAndInches, "2 feet")]
+    [InlineData(63, StringExtensions.MeasurementDisplayType.FeetAndInches, "2 feet 1 inch")]
+    [InlineData(89, StringExtensions.MeasurementDisplayType.FeetAndInches, "2 feet 11 inches")]
+    [InlineData(999, StringExtensions.MeasurementDisplayType.FeetAndInches, "32 feet 9 inches")]
+    internal Task TopicName_Returns_ExpectedTopicName_ForConfiguredSuffix(int? cm, StringExtensions.MeasurementDisplayType mdt, string expectedResult)
+    {
+        var actualResult = cm.ConvertMeasurementToDisplayString(mdt);
+
+        // Assert
+        Assert.Equal(expectedResult, actualResult);
+        return Task.CompletedTask;
+    }
+}
