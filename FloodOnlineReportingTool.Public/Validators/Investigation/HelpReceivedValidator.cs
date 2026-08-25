bca11fb436a6e4109c0803f4ff433676379a1293
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2026-08-25 09:00:54 +0100

    Updated checkboxes on HelpReceived page

--- FloodOnlineReportingTool.Public/Validators/Investigation/HelpReceivedValidator.cs
+++ FloodOnlineReportingTool.Public/Validators/Investigation/HelpReceivedValidator.cs
@@ -8,7 +8,7 @@ public class HelpReceivedValidator : AbstractValidator<HelpReceived>
     public HelpReceivedValidator()
     {
         RuleFor(o => o.HelpReceivedOptions)
-            .Must(o => o.Any(x => x.Selected))
+            .NotEmpty()
             .WithMessage("Select what help you received or select 'No help'");
     }
 }
